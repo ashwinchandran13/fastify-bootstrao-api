c7d9155fd54f8f8dc987fc4fd3d436f97975bf76
Ashwin Chandran <ashwinchandran13@gmail.com>
2021-08-05 19:52:04 +0530

    added swagger, schema for healthcheck

--- routes/healthCheck.js
+++ routes/healthCheck.js
@@ -4,8 +4,21 @@ module.exports = async function (fastify, opts) {
   fastify.route({
     method: 'GET',
     url: '/',
+    schema: {
+      tags: ['Healthcheck'],
+      description: 'Healthcheck enpoint to determine if service is up and running',
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            status: { type: 'string' },
+            timestamp: { type: 'string', format: 'date-time' }
+          } 
+        }
+      },
+    },
     handler: async (request, reply) => {
-      return { root: true }
+      return { status: 'ok', timestamp: new Date().toISOString() };
     }
-  })
+  });
 }
